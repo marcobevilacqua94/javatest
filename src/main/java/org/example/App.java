1ed397fcb3f24c6bf8b28b7f6054d0ce0a52163f
marcobevilacqua94 <marco.bevilacqua@couchbase.com>
2024-09-18 16:04:51 +0200

    zxc

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import com.couchbase.client.core.cnc.events.transaction.TransactionLogEvent;
 import com.couchbase.client.core.env.LoggerConfig;
 import com.couchbase.client.core.error.DocumentExistsException;
 import com.couchbase.client.core.error.DocumentNotFoundException;
@@ -7,7 +8,11 @@ import com.couchbase.client.core.msg.kv.DurabilityLevel;
 import com.couchbase.client.java.*;
 import com.couchbase.client.java.json.JsonObject;
 import com.couchbase.client.java.transactions.TransactionGetResult;
+import com.couchbase.client.java.transactions.TransactionResult;
+import com.couchbase.client.java.transactions.config.TransactionOptions;
 import com.couchbase.client.java.transactions.config.TransactionsConfig;
+import com.couchbase.client.java.transactions.error.TransactionCommitAmbiguousException;
+import com.couchbase.client.java.transactions.error.TransactionFailedException;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
@@ -16,6 +21,7 @@ import java.io.IOException;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.*;
 
 
@@ -23,6 +29,7 @@ import java.util.logging.*;
  * Hello world!
  */
 public class App {
+    static Logger logger = LogManager.getLogManager().getLogger("global");
     private static String RandomString(int length) {
         String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
         StringBuilder sb = new StringBuilder(length);
@@ -37,25 +44,31 @@ public class App {
 
         System.out.println("Hello World!");
 
-
+        try {
+            logger.addHandler(new FileHandler("./logs.txt"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
         JsonObject jsonObject = JsonObject.create();
 
         jsonObject.put("body", RandomString(Integer.parseInt(args[4])));
         jsonObject.put("version", args[5]);
 
-        long startTime = System.nanoTime();
+
         try (Cluster cluster = Cluster.connect(
                 args[0],
                 ClusterOptions.clusterOptions(args[1], args[2])
 
-                        .environment(env -> env.transactionsConfig(TransactionsConfig.builder()
+                        .environment(env -> {env.transactionsConfig(TransactionsConfig.builder()
                                 .timeout(Duration.ofMinutes(10))
                                 .durabilityLevel(DurabilityLevel.NONE)
-                                .build()))
+                                .build()); env.ioConfig().numKvConnections(64);})
         )
 
         ) {
-
+            bulkTransactionReactive(jsonObject, cluster, args, true);
+            Thread.sleep(5000);
+            long startTime = System.nanoTime();
             switch(Integer.parseInt(args[7])) {
                 case 0:
                     bulkInsert(jsonObject, cluster, args);
@@ -67,7 +80,7 @@ public class App {
                     bulkTransaction(jsonObject, cluster, args);
                     break;
                 case 3:
-                    bulkTransactionReactive(jsonObject, cluster, args);
+                    bulkTransactionReactive(jsonObject, cluster, args, false);
                     break;
                 case 4:
                     bulkTransactionWithBuffer(jsonObject, cluster, args);
@@ -83,6 +96,8 @@ public class App {
             long endTime = System.nanoTime();
             long duration = (endTime - startTime);
             System.out.println(duration / 1000000000 + "s");
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
     }
 
@@ -134,24 +149,61 @@ public class App {
                 .block();
     }
 
-    public static void bulkTransactionReactive(JsonObject jsonObject, Cluster cluster, String[] args) {
+    public static void bulkTransactionReactive(JsonObject jsonObject, Cluster cluster, String[] args, boolean warmup) {
+
+        String collectionName = "test";
+        int num;
+        if(warmup){
+            collectionName = "warmup";
+            num = 500;
+        } else {
+            num = Integer.parseInt(args[3]);
+        }
         ReactiveBucket bucket = cluster.bucket("test").reactive();
         bucket.waitUntilReady(Duration.ofSeconds(10)).block();
-        ReactiveCollection coll = bucket.scope("test").collection("test");
+        ReactiveCollection coll = bucket.scope("test").collection(collectionName);
+        int concurrency = Runtime.getRuntime().availableProcessors() * 8;
+        AtomicInteger inFlight = new AtomicInteger(0);
+        JsonObject jsonObject1 = JsonObject.create();
 
-        int concurrency = Runtime.getRuntime().availableProcessors() * 8; // This many operations will be in-flight at once
-
-
-        cluster.reactive().transactions().run((ctx) -> Flux.range(0, Integer.parseInt(args[3]))
+        TransactionResult result = cluster.reactive().transactions().run((ctx) -> Flux.range(0, num)
                 .parallel(concurrency)
                 .runOn(Schedulers.boundedElastic())
                 .concatMap(
                         docId -> {
-                            if (docId % 1000 == 0) System.out.println(docId);
-                            return ctx.insert(coll, docId.toString(), jsonObject);
+                            int x = inFlight.incrementAndGet();
+                            if (docId % 50 == 0) System.out.println("Ferrari: " + docId + " " + x);
+                            return ctx.insert(coll, docId.toString(), jsonObject1)
+                                    .doOnNext(v -> inFlight.decrementAndGet());
                         }
-                ).then().retry()
-        ).then().retry().block();
+                ).then(), TransactionOptions.transactionOptions().timeout(Duration.ofMinutes(10))).block();
+//
+//        int concurrency = Runtime.getRuntime().availableProcessors() * 8; // This many operations will be in-flight at once
+//
+//        Long start = System.nanoTime();
+//        TransactionResult result = cluster.reactive().transactions().run((ctx) -> Flux.range(0, num)
+//                .parallel(concurrency)
+//                .runOn(Schedulers.boundedElastic())
+//                .concatMap(
+//                        docId -> {
+//                            if (docId % 1000 == 0) {System.out.println(docId); System.out.println("millisecs " + (System.nanoTime() - start) / 1000000);}
+//                            return ctx.insert(coll, docId.toString(), jsonObject);
+//
+//                        }
+//                ).then()
+//        ).doOnError(err -> {
+//            if (err instanceof TransactionCommitAmbiguousException) {
+//                throw logCommitAmbiguousError((TransactionCommitAmbiguousException) err);
+//            } else if (err instanceof TransactionFailedException) {
+//                throw logFailure((TransactionFailedException) err);
+//            }
+//        }).block();
+//        try {
+//            logger.addHandler(new FileHandler("./transactionlog.txt"));
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+        result.logs().forEach(message -> logger.info(message.toString()));
     }
 
     public static void bulkTransactionReactiveWithBuffer(JsonObject jsonObject, Cluster cluster, String[] args) {
@@ -269,5 +321,28 @@ public class App {
         ).then().retry().block();
 
     }
+
+    public static RuntimeException logCommitAmbiguousError(TransactionCommitAmbiguousException err) {
+        // This example will intentionally not compile: the application needs to use
+        // its own logging system to replace `logger`.
+        logger.warning("Transaction possibly reached the commit point");
+
+        for (TransactionLogEvent msg : err.logs()) {
+            logger.warning(msg.toString());
+        }
+
+        return err;
+    }
+
+    public static RuntimeException logFailure(TransactionFailedException err) {
+        logger.warning("Transaction did not reach commit point");
+
+        for (TransactionLogEvent msg : err.logs()) {
+            logger.warning(msg.toString());
+        }
+
+        return err;
+    }
+
 
 }
